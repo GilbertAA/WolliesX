e7a71709b86d09d1e4dc89b5e6d12782689749ea
Gilbert Aliwarga <galiwarga@willowinc.com>
2020-09-10 09:10:55 +1000

    commented test code

--- WebAPI/Controllers/TrolleyController.cs
+++ WebAPI/Controllers/TrolleyController.cs
@@ -29,52 +29,52 @@ namespace WebAPI.Controllers
                 return StatusCode((int)HttpStatusCode.InternalServerError);
             }
         }
-        [HttpGet]
-        [Route("Test")]
-        public IActionResult CalculateTrolleyTotal()
-        {
-            //{
-            //    "products": [
-            //    {
-            //        "name": "string",
-            //        "price": 0
-            //    }
-            //    ],
-            //    "specials": [
-            //    {
-            //        "quantities": [
-            //        {
-            //            "name": "string",
-            //            "quantity": 0
-            //        }
-            //        ],
-            //        "total": 0
-            //    }
-            //    ],
-            //    "quantities": [
-            //    {
-            //        "name": "string",
-            //        "quantity": 0
-            //    }
-            //    ]
-            //}
-            try
-            {
-                var input = new TrolleyInput
-                {
-                    products = new List<TrolleyProduct> {new TrolleyProduct {name = "test", price = 0}},
-                    quantities = new List<Quantity> {new Quantity {name = "test", quantity = 0}},
-                    specials = new List<Special> {new Special()}
-                };
-                input.specials[0].quantities = new List<Quantity>{new Quantity{name = "test", quantity = 0}};
-                input.specials[0].total = 0;
-                return Ok(_trolleyService.CalculateTrolley(input));
-            }
-            catch (System.Exception)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError);
-            }
-        }
+        //[HttpGet]
+        //[Route("Test")]
+        //public IActionResult CalculateTrolleyTotal()
+        //{
+        //    //{
+        //    //    "products": [
+        //    //    {
+        //    //        "name": "string",
+        //    //        "price": 0
+        //    //    }
+        //    //    ],
+        //    //    "specials": [
+        //    //    {
+        //    //        "quantities": [
+        //    //        {
+        //    //            "name": "string",
+        //    //            "quantity": 0
+        //    //        }
+        //    //        ],
+        //    //        "total": 0
+        //    //    }
+        //    //    ],
+        //    //    "quantities": [
+        //    //    {
+        //    //        "name": "string",
+        //    //        "quantity": 0
+        //    //    }
+        //    //    ]
+        //    //}
+        //    try
+        //    {
+        //        var input = new TrolleyInput
+        //        {
+        //            products = new List<TrolleyProduct> {new TrolleyProduct {name = "test", price = 0}},
+        //            quantities = new List<Quantity> {new Quantity {name = "test", quantity = 0}},
+        //            specials = new List<Special> {new Special()}
+        //        };
+        //        input.specials[0].quantities = new List<Quantity>{new Quantity{name = "test", quantity = 0}};
+        //        input.specials[0].total = 0;
+        //        return Ok(_trolleyService.CalculateTrolley(input));
+        //    }
+        //    catch (System.Exception)
+        //    {
+        //        return StatusCode((int)HttpStatusCode.InternalServerError);
+        //    }
+        //}
 
     }
 }
